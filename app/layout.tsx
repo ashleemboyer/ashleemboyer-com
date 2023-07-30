706f01c4371afce2a90d86e936a1dbf26079c4ca
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-07-30 14:29:03 -0400

    Adding site header and array of links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { ReactNode } from "react";
 
+const SITE_LINKS = [
+  { href: "/about", text: "about" },
+  { href: "/blog", text: "blog" },
+  { href: "/rss", text: "rss" },
+  { href: "/talks", text: "talks" },
+  { href: "/resume", text: "resume" },
+];
+
 interface RootLayoutProps {
   children: ReactNode;
 }
@@ -7,7 +15,25 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <a href="/">
+            <img alt="home" src="amb.svg" height="48" width="auto" />
+          </a>
+          <nav>
+            <ul>
+              {SITE_LINKS.map(({ href, text }) => {
+                return (
+                  <li key={href}>
+                    <a href={href}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </header>
+        <div>{children}</div>
+      </body>
     </html>
   );
 };
