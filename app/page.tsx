db8ac50a005875b12e8755d7e3f3cfdf1827c7b6
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-07-30 14:29:16 -0400

    Adding home page heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 const Page = () => {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      {/* TODO write comment about chosen HTML, referencing: https://www.tpgi.com/subheadings-subtitles-alternative-titles-and-taglines-in-html/ */}
+      <hgroup>
+        <h1>Ashlee M Boyer</h1>
+        <p>
+          Disabled & Neurodivergent Software Engineer and Web Accessibility
+          Expert
+        </p>
+      </hgroup>
+    </>
+  );
 };
 
 export default Page;
