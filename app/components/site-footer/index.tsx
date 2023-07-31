3f2f27dceb2778acaebba864c751b3acdd297c44
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-07-30 21:08:15 -0400

    Adding SiteFooter

---
+++ app/components/site-footer/index.tsx
@@ -0,0 +1,14 @@
+const SiteFooter = () => {
+  return (
+    <footer>
+      <p>© 2023 Ashlee M Boyer</p>
+      <p>
+        <a href="https://github.com/ashleemboyer/ashleemboyer-com-issues-and-questions/issues/new/choose">
+          Report an issue or ask a question
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export { SiteFooter };
