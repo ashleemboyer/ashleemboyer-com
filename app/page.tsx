eeab349d44d189adb7d646f6266629f88cd6570b
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-07-30 21:39:21 -0400

    Using PageTitleGroup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
+import { PageTitleGroup } from "./components";
+
 const Page = () => {
   return (
     <>
-      {/* TODO write comment about chosen HTML, referencing: https://www.tpgi.com/subheadings-subtitles-alternative-titles-and-taglines-in-html/ */}
-      <hgroup>
-        <h1>Ashlee M Boyer</h1>
-        <p>
-          Disabled & Neurodivergent Software Engineer and Web Accessibility
-          Expert
-        </p>
-      </hgroup>
+      <PageTitleGroup
+        title="Ashlee M Boyer"
+        subtitle="Disabled & Neurodivergent Software Engineer and Web Accessibility Expert"
+      />
     </>
   );
 };
