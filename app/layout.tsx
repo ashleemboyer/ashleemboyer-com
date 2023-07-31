bd8e0991bd2d402de1614527abf3ccbbbf4310a6
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-07-30 21:08:53 -0400

    Using new components and minor layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import { ReactNode } from "react";
+import { SiteFooter, SiteHeader } from "./components";
 import "./global.css";
-
-const SITE_LINKS = [
-  { href: "/about", text: "about" },
-  { href: "/blog", text: "blog" },
-  { href: "/rss", text: "rss" },
-  { href: "/talks", text: "talks" },
-  { href: "/resume", text: "resume" },
-];
+import s from "./root-layout.module.css";
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -16,34 +10,10 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
-      <body>
-        {/* TODO move header element to separate component */}
-        <header>
-          <a href="/">
-            <img alt="home" src="amb.svg" height="48" width="auto" />
-          </a>
-          <nav>
-            <ul>
-              {SITE_LINKS.map(({ href, text }) => {
-                return (
-                  <li key={href}>
-                    <a href={href}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </header>
+      <body className={s.body}>
+        <SiteHeader />
         <div>{children}</div>
-        {/* TODO move footer element to separate component */}
-        <footer>
-          <p>© 2023 Ashlee M Boyer</p>
-          <p>
-            <a href="https://github.com/ashleemboyer/ashleemboyer-com-issues-and-questions/issues/new/choose">
-              Report an issue or ask a question
-            </a>
-          </p>
-        </footer>
+        <SiteFooter />
       </body>
     </html>
   );
