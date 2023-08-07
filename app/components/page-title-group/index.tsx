0f553b61f9c5858a325e4db31354f39c287d5a1d
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-08-06 20:35:06 -0400

    Adding notes and semantics to PageTitleGroup

--- app/components/page-title-group/index.tsx
+++ app/components/page-title-group/index.tsx
@@ -6,14 +6,35 @@ interface PageTitleGroupProps {
 }
 
 /**
- * TODO write comment about chosen HTML, referencing:
- * https://www.tpgi.com/subheadings-subtitles-alternative-titles-and-taglines-in-html/
+ * Notes on the chosen HTML and ARIA (as of August 6th, 2023):
+ *
+ * - Without `role` or `aria-roledescription` set on the `hgroup` element, it is
+ *   not exposed in any interesting way to assitive technology in the
+ *   accessibilty tree.
+ * - With `role` and `aria-description` set on the `hgroup` element and
+ *   `aria-roledescription` set on the `p` element, ambiguity about these
+ *   elements and their relation to one another is removed.
+ * - Until browsers add more semantics to the `hgroup` element, the guidance
+ *   today for subtitles/subheadings is to provide information to assistive
+ *   technologies. They might not use it, but that is up to their developers.
+ *
+ * Resources:
+ *  - https://www.tpgi.com/subheadings-subtitles-alternative-titles-and-taglines-in-html/
+ *  - https://www.a11yproject.com/posts/how-to-accessible-heading-structure/#hgroup
+ *  - https://www.w3.org/TR/wai-aria-1.2/#dfn-accessibility-tree
+ *  - https://www.w3.org/TR/wai-aria-1.2/#aria-roledescription
  */
 const PageTitleGroup = ({ title, subtitle }: PageTitleGroupProps) => {
   return (
-    <hgroup className={s.hgroup}>
+    <hgroup
+      role="group"
+      aria-roledescription="heading group"
+      className={s.hgroup}
+    >
       <h1 className={s.title}>{title}</h1>
-      <p className={s.subtitle}>{subtitle}</p>
+      <p aria-roledescription="subtitle" className={s.subtitle}>
+        {subtitle}
+      </p>
     </hgroup>
   );
 };
