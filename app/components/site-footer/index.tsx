907e005dbf1141cff4a13f63788de800fe74974f
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2023-08-06 20:45:40 -0400

    Adding TODO to footer link

--- app/components/site-footer/index.tsx
+++ app/components/site-footer/index.tsx
@@ -5,6 +5,7 @@ const SiteFooter = () => {
     <footer className={s.footer}>
       <p>© 2023 Ashlee M Boyer</p>
       <p>
+        {/* @TODO update link once ashleemboyer-com repo has issue templates */}
         <a href="https://github.com/ashleemboyer/ashleemboyer-com-issues-and-questions/issues/new/choose">
           Report an issue or ask a question
         </a>
